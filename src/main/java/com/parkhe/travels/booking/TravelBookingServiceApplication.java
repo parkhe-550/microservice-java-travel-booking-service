cc3fc2a945a3d0efa4d4abc82463389befe5cdd4
parkhe-550 <prof.parkhe.550@gmail.com>
2023-02-25 17:38:02 +0530

    Project Setup

---
+++ src/main/java/com/parkhe/travels/booking/TravelBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parkhe.travels.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkhe/travels/booking/TravelBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhe.travels.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
